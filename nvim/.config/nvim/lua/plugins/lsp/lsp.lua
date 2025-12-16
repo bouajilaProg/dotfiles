800fb05b00870b935017959388262de796224b53
bouajilaprog <bouajilamedyessine@gmail.com>
2025-12-16 11:40:26 +0100

    refactor: nvim make each lsp plugin in their own file

---
+++ nvim/.config/nvim/lua/plugins/lsp/lsp.lua
@@ -0,0 +1,17 @@
+-- this folder is for configuring language servers of lspconfig
+
+-- web
+vim.lsp.enable("ts_ls")
+vim.lsp.enable("html")
+vim.lsp.enable("tailwindcss")
+vim.lsp.enable("cssls")
+
+-- python
+vim.lsp.enable("pylsp")
+
+-- utils
+vim.lsp.enable("jsonls")
+vim.lsp.enable("yamlls")
+
+-- other
+vim.lsp.enable("copilot")
---
+++ nvim/.config/nvim/lua/plugins/lsp/mason.lua
@@ -0,0 +1,22 @@
+return {
+	{
+		"mason-org/mason.nvim",
+		opts = {
+			ensure_installed = {
+				"stylua",
+				"shellcheck",
+				"shfmt",
+				"flake8",
+				"rust-analyzer",
+				"bash-language-server",
+				"python-lsp-server",
+				-- web
+				"html-lsp",
+				"tailwindcss-language-server",
+				"css-lsp",
+				"typescript-language-server",
+				"intelephense",
+			},
+		},
+	},
+}
