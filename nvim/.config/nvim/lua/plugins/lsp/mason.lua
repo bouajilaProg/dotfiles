26a55f4bd081c662b9f0ec942176ec1da704fb1d
bouajilaprog <bouajilamedyessine@gmail.com>
2025-12-16 20:25:52 +0100

    refactor(nvim): move all the mason related plugins in one file

--- nvim/.config/nvim/lua/plugins/lsp/mason.lua
+++ nvim/.config/nvim/lua/plugins/lsp/mason.lua
@@ -1,22 +1,48 @@
 return {
 	{
-		"mason-org/mason.nvim",
+		"mason-org/mason-lspconfig.nvim",
 		opts = {
 			ensure_installed = {
-				"stylua",
-				"shellcheck",
-				"shfmt",
-				"flake8",
-				"rust-analyzer",
-				"bash-language-server",
-				"python-lsp-server",
-				-- web
-				"html-lsp",
-				"tailwindcss-language-server",
-				"css-lsp",
-				"typescript-language-server",
-				"intelephense",
+				"bashls",
+				"cssls",
+				"html",
+				"pyright",
+				"rust_analyzer",
+				"tailwindcss",
+				"tsserver",
 			},
+		},
+
+		dependencies = {
+			{
+				"mason-org/mason.nvim",
+				opts = {
+					ui = {
+						border = "rounded",
+						icons = {
+							package_installed = "✓",
+							package_pending = "➜",
+							package_uninstalled = "✗",
+						},
+					},
+					ensure_installed = {
+						"stylua",
+						"shellcheck",
+						"shfmt",
+						"flake8",
+						"rust-analyzer",
+						"bash-language-server",
+						"python-lsp-server",
+						-- web
+						"html-lsp",
+						"tailwindcss-language-server",
+						"css-lsp",
+						"typescript-language-server",
+					},
+				},
+			},
+
+			"neovim/nvim-lspconfig",
 		},
 	},
 }
