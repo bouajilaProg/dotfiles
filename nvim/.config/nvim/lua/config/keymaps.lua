50757c53867d45eff08a981574e326f6fa3707dc
bouajilaprog <bouajilamedyessine@gmail.com>
2025-12-16 11:38:36 +0100

    feat: nvim add windows navigation with arrow keys

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -3,3 +3,10 @@ vim.api.nvim_set_keymap("i", "<C-l>", 'copilot#Accept("<CR>")', { silent = true,
 
 -- go back a word with shift + e
 vim.api.nvim_set_keymap("n", "E", "b", { noremap = true })
+
+-- window navigation
+vim.api.nvim_set_keymap("n", "<leader>w<Left>", "<C-w>h", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>w<Down>", "<C-w>j", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>w<Up>", "<C-w>k", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>w<Right>", "<C-w>l", { noremap = true, silent = true })
+
