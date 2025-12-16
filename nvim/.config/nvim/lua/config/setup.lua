f8829b8a71ae8f922419e397c78c3eb7b363ebd8
bouajilaprog <bouajilamedyessine@gmail.com>
2025-12-16 20:22:48 +0100

    refactor(nvim): put the lsp enable langauge servers in the setup

--- nvim/.config/nvim/lua/config/setup.lua
+++ nvim/.config/nvim/lua/config/setup.lua
@@ -13,3 +13,19 @@ require("telescope").setup({
 	},
 })
 vim.g.lazyvim_picker = "telescope"
+
+-- web
+vim.lsp.enable("ts_ls")
+vim.lsp.enable("html")
+vim.lsp.enable("tailwindcss")
+vim.lsp.enable("cssls")
+
+-- python
+vim.lsp.enable("pylsp")
+
+-- utils
+vim.lsp.enable("jsonls")
+vim.lsp.enable("yamlls")
+
+-- other
+vim.lsp.enable("copilot")
