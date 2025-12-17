c4ef527b4dc7d0533bad5321c5a69f8b94eca084
bouajilaprog <bouajilamedyessine@gmail.com>
2025-12-17 22:38:12 +0100

    feat(shell): add nvm(node version manager)

--- scripts/scripts/bashConfig/exports.sh
+++ scripts/scripts/bashConfig/exports.sh
@@ -9,3 +9,7 @@ export DOCKER_CONTENT_TRUST=1
 
 # scripts variables
 export caffeine=0
+
+export NVM_DIR="$HOME/.config/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
