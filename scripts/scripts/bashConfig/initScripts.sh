feeeda6f3e8098f43f0563d86194a08c1171cd9b
bouajilaprog <bouajilamedyessine@gmail.com>
2025-12-24 13:31:59 +0100

    refactor(shell): remove conda init commented code

--- scripts/scripts/bashConfig/initScripts.sh
+++ scripts/scripts/bashConfig/initScripts.sh
@@ -20,16 +20,3 @@ export NVM_DIR="$HOME/.config/nvm"
 
 # set up zoxide
 eval "$(zoxide init bash)"
-
-# >>> conda initialize >>>
-#__conda_setup="$('/home/bouajila/miniconda3/bin/conda' 'shell.bash' 'hook' 2>/dev/null)"
-#if [ $? -eq 0 ]; then
-#  eval "$__conda_setup"
-#else
-#  if [ -f "/home/bouajila/miniconda3/etc/profile.d/conda.sh" ]; then
-#    . "/home/bouajila/miniconda3/etc/profile.d/conda.sh"
-#  else
-#    export PATH="/home/bouajila/miniconda3/bin:$PATH"
-#  fi
-#fi
-#unset __conda_setup
