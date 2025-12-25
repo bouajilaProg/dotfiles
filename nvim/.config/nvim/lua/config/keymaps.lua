a5adb23adfee5c47efd6997a764a147c52a7e451
bouajilaprog <bouajilamedyessine@gmail.com>
2025-12-25 21:40:59 +0100

    feat(nvim): make the alt + up/down arrows move the lines
    used to be in vs code and i didn't have it here
    hihihi not anymore

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -24,6 +24,11 @@ vim.keymap.set({ "n", "v" }, "d", "d", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("v", "p", '"_dP', { noremap = true })
 vim.api.nvim_set_keymap("v", "P", '"_dP', { noremap = true })
 
+-- move lines up and down
+vim.api.nvim_set_keymap("n", "<A-Down>", ":m .+1<CR>==", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<A-Up>", ":m .-2<CR>==", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<A-Down>", ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<A-Up>", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
 
 -- trouble
 local trouble_maps = {
