9d17ec99c7053cf8afcf696306e31598fe6389c7
bouajilaprog <bouajilamedyessine@gmail.com>
2025-12-26 21:16:30 +0100

    refactor(programs[wifi-share]): rewrite hotspot script with config sourcing and safer control flow

--- programs/programs/utils/wifi-share/wifi-share.sh
+++ programs/programs/utils/wifi-share/wifi-share.sh
@@ -1,92 +1,143 @@
 #!/bin/bash
 
+set -euo pipefail
 exec 2>/dev/null
-name=$(head -n 1 ~/bouajilascripts/utils/wifi-share/wifi.conf | tr "=" "\n" | tail -n 1)
-interface=$(head -n 3 ~/bouajilascripts/utils/wifi-share/wifi.conf | tail -n 1 | tr "=" "\n" | tail -n 1)
-password=$(head -n 4 ~/bouajilascripts/utils/wifi-share/wifi.conf | tail -n 1 | tr "=" "\n" | tail -n 1)
 
-function readConf() {
-  name=$(head -n 1 ~/bouajilascripts/utils/wifi-share/wifi.conf | tr "=" "\n" | tail -n 1)
-  interface=$(head -n 3 ~/bouajilascripts/utils/wifi-share/wifi.conf | tail -n 1 | tr "=" "\n" | tail -n 1)
-  password=$(head -n 4 ~/bouajilascripts/utils/wifi-share/wifi.conf | tail -n 1 | tr "=" "\n" | tail -n 1)
-}
+CONF="$HOME/bouajilascripts/utils/wifi-share/wifi.conf"
+CONF="${BASH_SOURCE[0]%/*}/wifi.conf"
 
-if [ ! -f ~/bouajilascripts/utils/wifi-share/wifi.conf ]; then
-  echo "wifi.conf not found"
+NAME=""
+INTERFACE=""
+PASSWORD=""
+
+die() {
+  echo "$1"
   exit 1
-fi
-
-function stopWifi() {
-  nmcli connection delete "$name"
 }
 
-function startWifi() {
-  readConf
-  if [ -n "$(nmcli device wifi list | grep 'zwawin')" ]; then
-    stopWifi
-  fi
-  nmcli connection add type wifi ifname "$interface" mode ap con-name "$name" ssid "$name" ipv4.method shared
-  nmcli connection modify "$name" 802-11-wireless-security.key-mgmt wpa-psk
-  nmcli connection modify "$name" 802-11-wireless-security.psk "$password"
-  nmcli connection modify "$name" 802-11-wireless.cloned-mac-address random
-  nmcli connection up "$name"
+read_conf() {
+  [[ -f "$CONF" ]] || die "wifi.conf not found"
+
+  # shellcheck disable=SC1090
+  source "$CONF"
+
+  NAME="${name:-}"
+  INTERFACE="${interface:-}"
+  PASSWORD="${passwd:-}"
+
+  [[ -n "$NAME" && -n "$INTERFACE" && -n "$PASSWORD" ]] ||
+    die "Invalid wifi.conf (missing values)"
 }
 
-case "$1" in
-"start")
-  echo "Starting the wireless network"
-  startWifi
+set_conf() {
+  local key="$1"
+  local value="$2"
+
+  sed -i "s|^$key=.*|$key=$value|" "$CONF"
+}
+
+wifi_exists() {
+  nmcli -t -f NAME connection show | grep -qx "$NAME"
+}
+
+wifi_stop() {
+  read_conf
+  wifi_exists && nmcli connection delete "$NAME"
+}
+
+wifi_start() {
+  read_conf
+
+  wifi_exists && wifi_stop
+
+  nmcli connection add \
+    type wifi \
+    ifname "$INTERFACE" \
+    mode ap \
+    con-name "$NAME" \
+    ssid "$NAME" \
+    ipv4.method shared
+
+  nmcli connection modify "$NAME" \
+    802-11-wireless-security.key-mgmt wpa-psk \
+    802-11-wireless-security.psk "$PASSWORD" \
+    802-11-wireless.cloned-mac-address random
+
+  nmcli connection up "$NAME"
+}
+
+wifi_restart() {
+  read_conf
+  nmcli connection down "$NAME" || true
+  nmcli connection up "$NAME"
+}
+
+wifi_info() {
+  nmcli dev wifi show
+}
+
+# Main
+
+case "${1:-}" in
+start)
+  echo "Starting Wi-Fi hotspot"
+  wifi_start
   ;;
 
-"stop")
-  echo "Stopping the wireless network $name"
-  stopWifi
+stop)
+  echo "Stopping Wi-Fi hotspot"
+  wifi_stop
   ;;
 
-"restart")
-  echo "Restarting the wireless network"
-  nmcli connection down "$name"
-  nmcli connection up "$name"
+restart)
+  echo "Restarting Wi-Fi hotspot"
+  wifi_restart
   ;;
 
-"set")
-  stopWifi
+set)
+  wifi_stop
   sleep 1
-  case "$2" in
-  "-n" | "--name")
-    echo "Setting the network name"
-    sed -i "s/name=.*/name=$3/" ~/bouajilascripts/utils/wifi-share/wifi.conf
+
+  case "${2:-}" in
+  -n | --name)
+    set_conf name "$3"
     ;;
-  "-i" | "--interface")
-    echo "Setting the network interface"
-    sed -i "s/interface=.*/interface=$3/" ~/bouajilascripts/utils/wifi-share/wifi.conf
+  -i | --interface)
+    set_conf interface "$3"
     ;;
-  "-p" | "--password")
-    echo "Setting the network password"
-    sed -i "s/passwd=.*/passwd=$3/" ~/bouajilascripts/utils/wifi-share/wifi.conf
+  -p | --password)
+    set_conf passwd "$3"
+    ;;
+  *)
+    die "Unknown set option"
     ;;
   esac
-  startWifi
 
+  wifi_start
   ;;
 
-"info")
-  nmcli dev wifi show
+info)
+  wifi_info
   ;;
 
-"help" | "-h")
-  echo "Usage: wifi-share.sh [start|stop|restart|set|users|help]"
-  echo "start: Start the wireless network"
-  echo "stop: Stop the wireless network"
-  echo "restart: Restart the wireless network"
-  echo "set: Set the network name, interface or password"
-  echo "  -n, --name: Set the network name"
-  echo "  -i, --interface: Set the network interface"
-  echo "  -p, --password: Set the network password"
-  echo "info: Show the wireless network information"
-  echo "help: Show this help message"
+help | -h)
+  cat <<EOF
+Usage: wifi-share [start|stop|restart|set|info|help]
+
+Commands:
+  start        Start hotspot
+  stop         Stop hotspot
+  restart      Restart hotspot
+  info         Show Wi-Fi info
+
+Set options:
+  set -n NAME        Set SSID
+  set -i INTERFACE   Set interface
+  set -p PASSWORD    Set password
+EOF
   ;;
+
 *)
-  echo "unkown command"
+  die "Unknown command"
   ;;
 esac
