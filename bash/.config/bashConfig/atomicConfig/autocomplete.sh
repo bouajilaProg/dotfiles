9971de2af6ac127fe30a68addae5c5fdaa6de121
bouajilaprog <bouajilamedyessine@gmail.com>
2026-01-15 16:57:04 +0100

    feat(bash): make auto-completion ignore case

---
+++ bash/.config/bashConfig/atomicConfig/autocomplete.sh
@@ -0,0 +1,7 @@
+# Bash Completion
+if [ -f /usr/share/bash-completion/bash_completion ]; then
+  source /usr/share/bash-completion/bash_completion
+fi
+
+# Case-insensitive completion
+bind 'set completion-ignore-case on'
